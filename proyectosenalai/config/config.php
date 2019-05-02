15aa97244989fadb9489ebd368eefc7c8834fa0f
modoSena <50187141+modoSena@users.noreply.github.com>
2019-05-02 07:02:09 -0500

    cac

--- proyectosenalai/config/config.php
+++ proyectosenalai/config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('URL', 'http://localhost/proyectosenalai/');
+define('URL', 'http://localhost/proyectoF/proyectosenalai/');
 define('HOST', '127.0.0.1');
 define('DB', 'senalai');
 define('USER', 'root');
