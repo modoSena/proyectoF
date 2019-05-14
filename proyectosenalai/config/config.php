d6f764fb90877c92aef71e048173d91e124d3620
vacilick <vacilickamp@gmail.com>
2019-05-14 09:05:25 -0500

    a

--- proyectosenalai/config/config.php
+++ proyectosenalai/config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('URL', 'http://localhost/proyectoF/proyectosenalai/');
+define('URL', 'http://localhost:8080/proyectoF/proyectosenalai/');
 define('HOST', '127.0.0.1');
 define('DB', 'senalai');
 define('USER', 'root');
