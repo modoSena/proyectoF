5527811cfac9c091dd82ee8374af517a1ac257e4
victorAdsi <48528632+victorAdsi@users.noreply.github.com>
2019-05-22 11:24:07 -0500

    aa

--- proyectosenalai/views/Prestamo/index.php
+++ proyectosenalai/views/Prestamo/index.php
@@ -30,9 +30,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Elementos del préstamo</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+        <h5 class="modal-title" id="exampleModalCenterTitle" style="text-align:center">Elementos del préstamo</h5>
         </button>
       </div>
       <div class="modal-body">
