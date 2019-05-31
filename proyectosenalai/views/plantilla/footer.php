8103fd8b66e9de40bacc51144f5aba334445597e
victorAdsi <48528632+victorAdsi@users.noreply.github.com>
2019-05-31 09:14:33 -0500

    views

--- proyectosenalai/views/plantilla/footer.php
+++ proyectosenalai/views/plantilla/footer.php
@@ -6,13 +6,17 @@
       <div class="col-md-3 col-sm-6 footer-col">
          <h6 class="heading7">
 SENA L.A.I.</h6>
-        <p>Se abre un mundo de posibilidades para aquellos individuos que hacemos uso de sus servicios, una de ellos es que ofrece la posibilidad de inventario de equipos, prestamos de equipos, auditorio y locker’s para fines educativos; así, resulta ser una herramienta de gran utilidad para la formación de profesionales, profesores y alumnos de todo tipo, ya que ofrece una organización de los elementos que este contiene</p>
+        <p>Realizado por:</p>
+        <p>- Cristian Mateo Florez Garcia</p>
+        <p>- Richard Sebastian Bentacur Villda</p>
+        <p>- Victor Hugo Rios Preciado</p>
+        <p>- Juan Camilo Hurtado Agudelo</p>
       </div>
       <div class="col-md-3 col-sm-6 footer-col">
         <h6 class="heading7">NUESTROS DATOS</h6>
         <p><i class="fa fa-map-pin"></i>Dirección: Cl. 51 # 57-70 - Torre Norte, Medellín, Antioquia </p>
-        <p><i class="fa fa-phone"></i> Telefono:  (colombia) :+57 1 800 0910270</p>
-        <p><i class="fa fa-envelope"></i> E-mail : gpservicioalcliente@sena.edu.co</p>
+        <p><i class="fa fa-phone"></i> Telefono:  (colombia) :+4 576-0000 Ext. 42194</p>
+        <p><i class="fa fa-envelope"></i> E-mail : senalai31@gmail.com</p>
         <ul class="footer-ul">
         </ul>
       </div>
