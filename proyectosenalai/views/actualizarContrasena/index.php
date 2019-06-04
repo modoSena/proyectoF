a1a8d3079932846eaf46c7275efd53cb277293aa
victorAdsi <48528632+victorAdsi@users.noreply.github.com>
2019-06-04 10:54:22 -0500

    vistas

--- proyectosenalai/views/actualizarContrasena/index.php
+++ proyectosenalai/views/actualizarContrasena/index.php
@@ -42,7 +42,7 @@
     <div class="row">
       <div  class="col-xs-6 col-md-6">
        <input type="hidden" name="envioActualizarContrasena">
-                <input id="submitActualizarContrasena" name="submitActualizarContrasena" type="button" value="Aceptar" class="btn btn-primary btn-block btn-lg" tabindex="7" >
+                <input id="submitActualizarContrasena" name="submitActualizarContrasena" type="button" value="Actualizar" class="btn btn-primary btn-block btn-lg" tabindex="7" >
               </div>
 
       <div  class="col-xs-6 col-md-6">
